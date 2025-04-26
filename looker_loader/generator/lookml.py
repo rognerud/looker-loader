950ad06652e10697bfef25f9d5c5a3184f4ebf7a
Gisle Rognerud <gisle.rognerud@gmail.com>
2025-04-26 23:40:21 +0200

    store

--- looker_loader/generator/lookml.py
+++ looker_loader/generator/lookml.py
@@ -2,6 +2,7 @@
 
 import os
 from typing import Dict
+from looker_loader.models.looker import Looker, LookerDim
 
 
 
@@ -20,37 +21,84 @@ class LookmlGenerator:
     def __init__(self, cli_args):
         self._cli_args = cli_args
     
-    def _generate_views(self, model, views = []):
-        """Generate LookML for a view."""
+    def _generate_views(self, model, views = [], parent=None):
+        """Split up the model into views"""
         
         view_dimensions = []
         view_measures = []
-        import logging
-        logging.info(model)
-        view_name = model.name
+
+        if parent is not None:
+            view_name = f"{parent}__{model.name}"
+        else:
+            view_name = model.name
 
         for field in model.fields:
             view_dimensions.append(field)
-            if field.measures is not None:
-                for measure in field.measures:
-                    view_measures.append(measure)
 
             if field.fields is not None:
-                self._generate_views(field, views)
+                self._generate_views(field, views, parent=view_name)
 
-        view = {
+        view = Looker(**{
             "name": view_name,
-            "dimensions": view_dimensions,
-            "measures": view_measures
-        }
+            "fields": view_dimensions,
+        })
         views.append(view)
 
         return views
 
+    def _extract_dimensions(self, model):
+        """Extract dimensions from the model"""
+        dimensions = []
+
+        import logging
+        logging.info(model)
+
+        for field in model.fields:
+            dimensions.append(field)
+            
+        return dimensions
+
+    def _extract_measures(self, model):
+        """Extract measures from the model"""
+        measures = []
+        for field in model.fields:
+            if field.measures is not None:
+                for measure in field.measures:
+                    measures.append(measure)
+        return measures
+
+    def _create_explore(self, model):
+        """Create an explore for the model"""
+        explore = {
+            "name": model.name,
+            "joins": []
+        }
+        return explore
+    
+
     def generate(self, model) -> Dict:
         """Generate LookML for a model."""
         
-        views = self._generate_views(model)
+        view_groups = self._generate_views(model)
 
-        for view in views:
-            
+        # explore = self._create_explore(model)
+
+        views = []
+        # for view in views:
+        for view in view_groups:
+            view_name = view.name
+            dimensions = self._extract_dimensions(view)
+            measures = self._extract_measures(view)
+
+            # Generate LookML for the view
+            lookml_view = {
+                "view": {
+                    "name": view_name,
+                    "dimensions": dimensions,
+                    "measures": measures
+                }
+            }
+
+            views.append(lookml_view)
+        
+        return views
